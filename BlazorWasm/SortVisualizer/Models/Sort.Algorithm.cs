eaf4cd499121b8aefdf017a25d9d6ecb3e4e6f70
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-10-18 14:38:56 +0900

    OutOfPlaceAlgorithm 要らない
    バッファー、メソッド内で new してしまえば外から渡す必要なく。
    new のアロケーションもったいないけど、まあ、デモだし。
    あと、 bucket sort は正確には Swap してないけどもまあ、Swap のロジック流用で。

--- BlazorWasm/SortVisualizer/Models/Sort.Algorithm.cs
+++ BlazorWasm/SortVisualizer/Models/Sort.Algorithm.cs
@@ -6,108 +6,128 @@ public partial class Sort
 
     private static readonly Algorithm[] _algorithms = new Algorithm[]
     {
-        I(QuickSort, "Quick", """
+        new(QuickSort)
+        {
+            Name = "Quick",
+            Description = """
             比較・スワップに基づくソートの中で、だいたいのデータで最速。
             時々苦手なデータあり。
-            """),
-
-        I(ShellSort, "Shell", """
+            """
+        },
+        new(ShellSort)
+        {
+            Name = "Shell",
+            Description = """
             Insertion 改良版。
             要素数がそんなに多くないうちはむっちゃ速い。
             O(N lon N) ではないけども、数百要素超えても Heap とか Merge よりも速い。
             速いわりにコードが短い。
-            """),
-
-        I(CombSort, "Comb", """
+            """
+        },
+        new(CombSort)
+        {
+            Name = "Comb",
+            Description = """
             Bubble 改良版。
             Insertion に対する Shell と似たような発想。
-            """),
-
-        I(HeapSort, "Heap", """
+            """
+        },
+        new(HeapSort)
+        {
+            Name = "Heap",
+            Description = """
             O(N log N) ソートの中では遅めだけども、苦手なデータがない。
             Quick ソートと相補的に使ったりする
             (再帰が深くなった時に切り替える)。
-            """),
-
-        I(InPlaceMergeSort, "In-place Merge", """
+            """
+        },
+        new(InPlaceMergeSort)
+        {
+            Name = "In-place Merge",
+            Description = """
             Merge 亜種。
             in-place (ソート対象の配列内で完結して、追加のメモリが不要)になるように変更したもの。
             その分遅い。
 
             Merge 自体、O(N log N) の中では遅い部類。
             ただ、安定ソートだったり並列化しやすいという利点あり。
-            """),
-
-        I(InsertSort, "Insertion", """
+            """
+        },
+        new(InsertSort)
+        {
+            Name = "Insertion",
+            Description = """
             遅いやつ。
             ただ、おおむねソート済みのデータに対しては速い。
             Quick ソートと相補的に使ったりする
             (データが短くなってきたときに切り替える)。
-            """),
-
-        I(CycleSort, "Cycle", """
+            """
+        },
+        new(CycleSort)
+        {
+            Name = "Cycle",
+            Description = """
             Selection 系。
             スワップ回数が理論上最小らしい。
-            """),
-
-        I(SelectSort, "Selection", """
+            """
+        },
+        new(SelectSort)
+        {
+            Name = "Selection",
+            Description = """
             遅いやつ。
             比較は多いけどもスワップは少ない。
-            """),
-
-        I(OddEvenSort, "Odd-Even", """
+            """
+        },
+        new(OddEvenSort)
+        {
+            Name = "Odd-Even",
+            Description = """
             Bubble 亜種。
             奇数・偶数に分けて Bubble ソート。
             奇数・偶数で並列処理可能なのがメリットらしい。
             正直、見た目が面白いから選んだ。
-            """),
-
-        I(ShakerSort, "Cocktail shaker", """
+            """
+        },
+        new(ShakerSort)
+        {
+            Name = "Cocktail shaker",
+            Description = """
             Bubble 亜種。
             行ったり来たりすることでスキャン範囲を狭める工夫とのこと。
             正直、見た目が面白いから選んだ。
-            """),
-
-        I(BubbleSort, "Bubble", """
+            """
+        },
+        new(BubbleSort)
+        {
+            Name = "Bubble",
+            Description = """
             入門記事によく出てくる遅いやつ。
-            """),
-
-        I(GnomeSort, "Gnome", """
+            """
+        },
+        new(GnomeSort)
+        {
+            Name = "Gnome",
+            Description = """
             とにかくコードが短い。
             まあ、その代わり最遅
             (わざと遅さを競うようなものは除く)。
-            """),
-
-        I(BucketSort, "Bucket", """
+            """
+        },
+        new(BucketSort)
+        {
+            Name = "Bucket",
+            Description = """
             適用できる条件がかなり限られている代わりに爆速。
             上限・下限が既知の整数配列とかに対して使うと本当に速い。 O(N)。
-            """),
+            """
+        },
     };
 
-    private static InPlaceAlgorithm I(Func<int[], IEnumerable<Operation>> sorter, string name, string? description = null)
-        => new(sorter)
-        {
-            Name = name,
-            Description = description,
-        };
-
-    public abstract record Algorithm
+    public record Algorithm(Func<int[], IEnumerable<Operation>> Sorter)
     {
         public required string Name { get; init; }
         public string? Description { get; init; }
-        public abstract IEnumerable<Operation> Start(int[] array);
-    }
-
-    public record InPlaceAlgorithm(Func<int[], IEnumerable<Operation>> Sorter) : Algorithm
-    {
-        public override IEnumerable<Operation> Start(int[] array) => Sorter(array);
-    }
-
-    public record OutOfPlaceAlgorithm<TBuffer>(
-        Func<int[], TBuffer> GetBuffer,
-        Func<int[], TBuffer, IEnumerable<Operation>> Sorter)
-        : Algorithm
-    {
-        public override IEnumerable<Operation> Start(int[] array) => Sorter(array, GetBuffer(array));
+        public IEnumerable<Operation> Start(int[] array) => Sorter(array);
     }
 }
