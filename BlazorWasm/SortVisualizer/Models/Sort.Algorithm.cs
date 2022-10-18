cf6c27212c1167f673483a8824226a2c2ac7b2a3
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-10-18 12:09:41 +0900

    CycleSort

--- BlazorWasm/SortVisualizer/Models/Sort.Algorithm.cs
+++ BlazorWasm/SortVisualizer/Models/Sort.Algorithm.cs
@@ -6,12 +6,13 @@ public partial class Sort
 
     private static readonly Algorithm[] _algorithms = new Algorithm[]
     {
-        I(QuickSort, "Quick Sort", "だいたいは最速。時々苦手なデータあり。"),
-        I(ShellSort, "Shell Sort", "要素数が多くないうちはむっちゃ速い。"),
-        I(HeapSort, "Heap Sort", "O(N log N) の中では遅めだけども、苦手なデータがない。Quick ソートと相補的に使ったりする。"),
-        I(InsertSort, "Insert Sort", "入門によく出る遅いやつ。ただ、おおむねソート済みのデータに対しては速い。"),
-        I(SelectSort, "Selection Sort", "入門によく出る遅いやつ。比較は多いけどもスワップは少ない。"),
-        I(OddEvenSort, "Odd-Even Sort", "Bubble 亜種。奇数・偶数に分けてソート。奇遇で並列処理可能なのがメリットらしい。"),
+        I(QuickSort, "Quick Sort", "比較・スワップに基づくソートの中で、だいたいは最速。時々苦手なデータあり。"),
+        I(ShellSort, "Shell Sort", "要素数がそんなに多くないうちはむっちゃ速い。速いわりにコードが短い。"),
+        I(HeapSort, "Heap Sort", "O(N log N) ソートの中では遅めだけども、苦手なデータがない。Quick ソートと相補的に使ったりする。"),
+        I(InsertSort, "Insert Sort", "遅いやつ。ただ、おおむねソート済みのデータに対しては速い。"),
+        I(CycleSort, "Cycle Sort", "Selection 系。スワップ回数が理論上最小らしい。"),
+        I(SelectSort, "Selection Sort", "遅いやつ。比較は多いけどもスワップは少ない。"),
+        I(OddEvenSort, "Odd-Even Sort", "Bubble 亜種。奇数・偶数に分けて Bubble ソート。奇遇で並列処理可能なのがメリットらしい。"),
         I(BubbleSort, "Bubble Sort", "入門によく出る遅いやつ。"),
         I(GnomeSort, "Gnome Sort", "コードが短い。まあ、遅い。"),
     };
