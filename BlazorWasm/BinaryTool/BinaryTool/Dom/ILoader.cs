e349240d7b24fd7339b3ee7bd9488c54b87f33c1
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-11-12 22:07:20 +0900

    DefaultLoaders

--- BlazorWasm/BinaryTool/BinaryTool/Dom/ILoader.cs
+++ BlazorWasm/BinaryTool/BinaryTool/Dom/ILoader.cs
@@ -1,6 +1,13 @@
+using System.Collections.Immutable;
+
 namespace BinaryTool.Dom;
 
 public interface ILoader : IDescriptiveItem
 {
     List<DomSpan> Parse(byte[] data);
+
+    public static readonly ImmutableArray<ILoader> DefaultLoaders = ImmutableArray.Create(new ILoader[]
+    {
+        JsonLoader.Instance,
+    });
 }
