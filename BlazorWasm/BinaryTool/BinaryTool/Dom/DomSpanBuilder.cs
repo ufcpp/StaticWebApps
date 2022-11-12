ac128b243d7e7ecba5ecabd359cba184d2286a0a
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-11-12 22:06:32 +0900

    DomSpanBuilder

---
+++ BlazorWasm/BinaryTool/BinaryTool/Dom/DomSpanBuilder.cs
@@ -0,0 +1,88 @@
+namespace BinaryTool.Dom;
+
+internal class DomSpanBuilder
+{
+    private readonly List<DomSpan> _results = new();
+    private readonly Stack<(object?, int startListIndex, int startPosition)> _hierarchy = new();
+    private string? _name;
+
+    public List<DomSpan> Results => _results;
+
+    public void Name(string name) => _name = name;
+
+    public void Add(object? value, Range range) => Add(DomKind.Value, _name, value, range);
+
+    public void Exception(Exception value, Range range) => Add(DomKind.Error, "$exception", value, range);
+
+    private void Add(DomKind kind, string? key, object? value, Range range)
+    {
+        var parent = Parent();
+
+        if (parent is Map m)
+        {
+            _results.Add(new(kind, range, value, key, 0));
+            m.Add(key!, value);
+        }
+        else if (parent is List l)
+        {
+            _results.Add(new(kind, range, value, l.Count, 0));
+            l.Add(value);
+        }
+        else
+        {
+            _results.Add(new(kind, range, value, null, 0));
+        }
+    }
+
+    private void Set(int index, DomKind kind, int length, object? value, Range range)
+    {
+        var parent = Parent();
+
+        if (parent is Map m)
+        {
+            _results[index] = new(kind, range, value, _name, length);
+            m.Add(_name!, value);
+        }
+        else if (parent is List l)
+        {
+            _results[index] = new(kind, range, value, l.Count, length);
+            l.Add(value);
+        }
+        else
+        {
+            _results[index] = new(kind, range, value, null, length);
+        }
+    }
+
+    private object? Parent()
+    {
+        var (parent, _, _) = _hierarchy.Count > 0 ? _hierarchy.Peek() : default;
+        return parent;
+    }
+
+    public void PushMap(int startPosition)
+    {
+        var obj = new Map();
+        var index = _results.Count;
+        _results.Add(default!);
+
+        _hierarchy.Push(new(obj, index, startPosition));
+    }
+
+    public void PushList(int startPosition)
+    {
+        var obj = new List();
+        var index = _results.Count;
+        _results.Add(default!);
+
+        _hierarchy.Push(new(obj, index, startPosition));
+    }
+
+    public void Pop(int endPosition)
+    {
+        var (obj, startListIndex, startPosition) = _hierarchy.Pop();
+
+        if (obj is Map map) Set(startListIndex, DomKind.Object, map.Count, map, startPosition..endPosition);
+        else if (obj is List list) Set(startListIndex, DomKind.Array, list.Count, list, startPosition..endPosition);
+    }
+}
--- BlazorWasm/BinaryTool/BinaryTool/Dom/JsonLoader.cs
+++ BlazorWasm/BinaryTool/BinaryTool/Dom/JsonLoader.cs
@@ -11,61 +11,13 @@ public class JsonLoader : ILoader
     public List<DomSpan> Parse(byte[] data)
     {
         var reader = new Utf8JsonReader(data);
-        var result = new List<DomSpan>();
-        Parse(ref reader, result, null);
-        return result;
+        var builder = new DomSpanBuilder();
+        Parse(ref reader, builder);
+        return builder.Results;
     }
 
-    private void Parse(ref Utf8JsonReader r, List<DomSpan> results, object? parent)
+    private static void Parse(ref Utf8JsonReader r, DomSpanBuilder builder)
     {
-        string? name = null;
-
-        void addValue(object? value, Range range) => add(DomKind.Value, value, range);
-        void addException(Exception value, Range range)
-        {
-            name = "$exception";
-            add(DomKind.Error, value, range);
-        }
-
-        void add(DomKind kind, object? value, Range range)
-        {
-            if (parent is Map m)
-            {
-                results.Add(new(kind, range, value, name, 0));
-                m.Add(name!, value);
-            }
-            else if (parent is List l)
-            {
-                results.Add(new(kind, range, value, l.Count, 0));
-                l.Add(value);
-            }
-            else
-            {
-                results.Add(new(kind, range, value, null, 0));
-            }
-        }
-
-        void setMap(int index, Map value, Range range) => set(index, DomKind.Object, value.Count, value, range);
-        void setList(int index, List value, Range range) => set(index, DomKind.Array, value.Count, value, range);
-
-        void set(int index, DomKind kind, int length, object? value, Range range)
-        {
-            if (parent is Map m)
-            {
-                results[index] = new(kind, range, value, name, length);
-                m.Add(name!, value);
-            }
-            else if (parent is List l)
-            {
-                results[index] = new(kind, range, value, l.Count, length);
-                l.Add(value);
-            }
-            else
-            {
-                results[index] = new(kind, range, value, null, length);
-            }
-        }
-
         while (true)
         {
             try
@@ -74,8 +26,8 @@ public class JsonLoader : ILoader
             }
             catch (JsonException ex)
             {
-                addException(ex, (int)r.BytesConsumed..);
-                return;
+                builder.Exception(ex, (int)r.BytesConsumed..);
+                continue;
             }
 
             switch (r.TokenType)
@@ -83,50 +35,35 @@ public class JsonLoader : ILoader
                 case JsonTokenType.None:
                     break;
                 case JsonTokenType.StartObject:
-                    {
-                        var obj = new Map();
-                        var index = results.Count;
-                        results.Add(default!);
-                        var start = (int)r.TokenStartIndex;
-                        Parse(ref r, results, obj);
-                        var end = (int)r.BytesConsumed;
-                        setMap(index, obj, start..end);
-                    }
+                    builder.PushMap((int)r.TokenStartIndex);
                     break;
                 case JsonTokenType.StartArray:
-                    {
-                        var obj = new List();
-                        var index = results.Count;
-                        results.Add(default!);
-                        var start = (int)r.TokenStartIndex;
-                        Parse(ref r, results, obj);
-                        var end = (int)r.BytesConsumed;
-                        setList(index, obj, start..end);
-                    }
+                    builder.PushList((int)r.TokenStartIndex);
                     break;
                 case JsonTokenType.EndObject:
                 case JsonTokenType.EndArray:
-                    return;
+                    builder.Pop((int)r.BytesConsumed);
+                    break;
                 case JsonTokenType.PropertyName:
-                    name = r.GetString();
+                    builder.Name(r.GetString()!);
                     break;
                 case JsonTokenType.Comment:
                     break;
                 case JsonTokenType.String:
-                    addValue(r.GetString(), r.Range());
+                    builder.Add(r.GetString(), r.Range());
                     break;
                 case JsonTokenType.Number:
                     object? num = r.TryGetInt64(out var l) ? l : r.TryGetDouble(out var d) ? d : null;
-                    addValue(num, r.Range());
+                    builder.Add(num, r.Range());
                     break;
                 case JsonTokenType.True:
-                    addValue(true, r.Range());
+                    builder.Add(true, r.Range());
                     break;
                 case JsonTokenType.False:
-                    addValue(false, r.Range());
+                    builder.Add(false, r.Range());
                     break;
                 case JsonTokenType.Null:
-                    addValue(null, r.Range());
+                    builder.Add(null, r.Range());
                     break;
             }
         }
