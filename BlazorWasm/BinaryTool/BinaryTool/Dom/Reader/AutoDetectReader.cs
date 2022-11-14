817e2a3785767bee2585a7ccf67e4221ae6f3cd0
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-11-14 22:58:45 +0900

    auto detect description (reader)

--- BlazorWasm/BinaryTool/BinaryTool/Dom/Reader/AutoDetectReader.cs
+++ BlazorWasm/BinaryTool/BinaryTool/Dom/Reader/AutoDetectReader.cs
@@ -2,31 +2,39 @@ namespace BinaryTool.Dom.Reader;
 
 public class AutoDetectReader : IReader
 {
-    public static readonly AutoDetectReader Instance = new();
+    private const string _desc = "auto detect";
 
-    public string Description => "auto detect";
+    public string Description { get; set; } = _desc;
 
     public (List<DomSpan> spans, bool isBinary) Read(byte[] data)
     {
-        var loaders = IReader.Readers;
+        var readers = IReader.Defaults;
 
+        IReader? maxItem = null;
         var maxCount = 0;
         (List<DomSpan> spans, bool isBinary) max = default;
 
-        for (int i = 0; i < loaders.Length; i++)
+        for (int i = 0; i < readers.Length; i++)
         {
-            var loader = loaders[i];
-            var t = loader.Read(data);
+            var reader = readers[i];
+            var t = reader.Read(data);
             var spans = t.spans;
 
-            if (spans.Count != 0 && spans[^1].Kind != DomKind.Error) return t;
+            if (spans.Count != 0 && spans[^1].Kind != DomKind.Error)
+            {
+                Description = $"{_desc} ({reader.Description})";
+                return t;
+            }
 
             if (spans.Count > maxCount)
             {
                 maxCount = spans.Count;
                 max = t;
+                maxItem = reader;
             }
         }
+
+        Description = $"{_desc} ({maxItem?.Description})";
 
         return max;
     }
